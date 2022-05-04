22950ea9117b142b1a893da873580ff44f4cb63c
root <root@TerraformLinuxServer.1qe23xihsgzuvg2x4itcyxdp1h.bx.internal.cloudapp.net>
2022-05-04 06:04:10 +0000

    code add

---
+++ vmprovision/variables.tf
@@ -0,0 +1,15 @@
+variable "name" {
+}
+variable "resource_group_name" {}
+variable "location" {}
+variable "size" {}
+variable "admin_username" {}
+variable "admin_password" {}
+variable "nic_name" {}
+variable "caching" {}
+variable "storage_account_type" {}
+variable "publisher" {}
+variable "offer" {}
+variable "sku" {}
+variable "version_name" {}
+
